f7a007d2bb3e1c79e1afb8eabe312b012000735e
Corey Smith <corsmith@gmail.com>
2020-05-19 17:59:49 -0400

    debug all requests

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
 import tornado.ioloop
 import tornado.web
+import tornado.options 
+from tornado.log import gen_log
+
+'''
+Alert Manager Documentation: https://prometheus.io/docs/alerting/configuration/
+
+Sample alertmanager message:
+
+{
+  "version": "4",
+  "groupKey": <string>,    // key identifying the group of alerts (e.g. to deduplicate)
+  "status": "<resolved|firing>",
+  "receiver": <string>,
+  "groupLabels": <object>,
+  "commonLabels": <object>,
+  "commonAnnotations": <object>,
+  "externalURL": <string>,  // backlink to the Alertmanager.
+  "alerts": [
+    {
+      "status": "<resolved|firing>",
+      "labels": <object>,
+      "annotations": <object>,
+      "startsAt": "<rfc3339>",
+      "endsAt": "<rfc3339>",
+      "generatorURL": <string> // identifies the entity that caused the alert
+    },
+    ...
+  ]
+}
+'''
 
 async def f():
     http_client = AsyncHTTPClient()
@@ -10,16 +40,23 @@ async def f():
     else:
         print(response.body)
 
-class MainHandler(tornado.web.RequestHandler):
+class HealthHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Hello, world\n")
+
+class MainHandler(tornado.web.RequestHandler):
+    def post(self, webhookkey):
+        gen_log.warning(f'webhookkey = { webhookkey }\nuri: { self.request.uri }\nquery: { self.request.query }\nheaders: { self.request.headers }\nbody: { self.request.body }')
+        self.write("Hello, %s\n" % webhookkey)
 
 def make_app():
     return tornado.web.Application([
-        (r"/", MainHandler),
+        (r"/v1/webhooks/incoming/([^/]+)", MainHandler),
+        (r"/", HealthHandler),
     ])
 
 if __name__ == "__main__":
+    tornado.options.parse_command_line()
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
